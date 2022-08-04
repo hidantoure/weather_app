5b16e925ae2d67cdfe1a6699ce3bb825e298ecf7
HamdiEzzahra <hidantoure@gmail.com>
2022-08-04 14:44:52 +0100

    added RemoteDataSource: abstract + implementation

--- lib/core/utils/constants.dart
+++ lib/core/utils/constants.dart
@@ -1,3 +1,4 @@
-class AppContant {
+class AppConstant {
   static const String baseUrl = 'https://api.openweathermap.org/data/2.5';
+  static const String apiKey = '751bb2a46eba45b1e061f5376886573f';
 }
---
+++ lib/weather/data/datasource/remote_data_source.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+
+import 'package:weather_app/core/utils/constants.dart';
+import 'package:weather_app/weather/data/models/weather_model.dart';
+import 'package:dio/dio.dart';
+
+abstract class BaseRemoteDataSource {
+  Future<WeatherModel?> getWeatherByCountryName(String countryName);
+}
+
+class RemoteDataSource implements BaseRemoteDataSource {
+  @override
+  Future<WeatherModel?> getWeatherByCountryName(String countryName) async {
+    try {
+      var response = await Dio().get(
+          '${AppConstant.baseUrl}/weather?q=$countryName&appid=${AppConstant.apiKey}');
+      print(response);
+      return WeatherModel.fromJson(json.decode(response.data));
+    } catch (e) {
+      print(e);
+      return null;
+    }
+  }
+}
